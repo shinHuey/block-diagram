d9177e1a3c669310a9c6d3ec14069fbaec982a8e
shinHuey <93418607+shinHuey@users.noreply.github.com>
2025-02-04 13:43:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def download_file(filename):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Render assigns a port, default to 10000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=Flase)
